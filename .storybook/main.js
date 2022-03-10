6bdacaa6023043b5e64f73a070fd20ad7f095b8a
Max Phillips <max.phillips@nine.com.au>
2022-03-10 22:42:51 +1100

    Change things

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,15 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+    {
+
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    }
   ],
   "framework": "@storybook/react"
 }
